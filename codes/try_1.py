4c833167974508c01afabd9ad09edbd6406c8190
furuiyang <furuiyang0715@gmail.com>
2020-06-08 12:29:34 +0800

    判断两张图片是否完全一致

---
+++ codes/try_1.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import math
+import operator
+from functools import reduce
+
+
+def image_contrast(img1, img2):
+
+    image1 = Image.open(img1)
+    image2 = Image.open(img2)
+
+    h1 = image1.histogram()
+    h2 = image2.histogram()
+
+    result = math.sqrt(reduce(operator.add,  list(map(lambda a, b: (a-b)**2, h1, h2)))/len(h1))
+    return result
+
+
+if __name__ == '__main__':
+    img1 = "./1.jpeg"  # 指定图片路径
+    img2 = "./2.jpeg"
+    result = image_contrast(img1, img2)
+    print(result)
