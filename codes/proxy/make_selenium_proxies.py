dfb5ce4ef26be8c3aa0237df68a9003c3c605485
furuiyang <furuiyang0715@gmail.com>
2021-03-01 14:02:09 +0800

    为 selenium 设置socket 代理

--- codes/proxy/make_selenium_proxies.py
+++ codes/proxy/make_selenium_proxies.py
@@ -61,3 +61,14 @@ browser = webdriver.Chrome(options=options)
 browser.get('https://httpbin.org/get')
 print(browser.page_source)
 browser.close()
+
+
+# 设置 socket 代理
+from selenium import webdriver
+proxy = '127.0.0.1:7891'
+options = webdriver.ChromeOptions()
+options.add_argument('--proxy-server=socks5://'+proxy)
+browser = webdriver.Chrome(options=options)
+browser.get('https://httpbin.org/get')
+print(browser.page_source)
+browser.close()
