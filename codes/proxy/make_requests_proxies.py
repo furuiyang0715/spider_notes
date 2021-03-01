aad01c599728a13b58c0fd10d5ec73d2c57066f6
furuiyang <furuiyang0715@gmail.com>
2021-03-01 13:21:05 +0800

    在 requests 中使用代理举例

---
+++ codes/proxy/make_requests_proxies.py
@@ -0,0 +1,41 @@
+import requests
+
+proxy = '127.0.0.1:7890'
+proxies = {'http': 'http://'+proxy,
+           'https': 'https://'+proxy
+           }
+
+try:
+    response = requests.get('https://httpbin.org/get',
+                            # proxies=proxies,    # 切换是否使用代理
+                            )
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
+
+
+# 运行结果：
+#
+# {
+#
+#  "args": {},
+#
+#  "headers": {
+#
+#    "Accept": "*/*",
+#
+#    "Accept-Encoding": "gzip, deflate",
+#
+#    "Host": "httpbin.org",
+#
+#    "User-Agent": "python-requests/2.22.0",
+#
+#    "X-Amzn-Trace-Id": "Root=1-5e8f358d-87913f68a192fb9f87aa0323"
+#
+#  },
+#
+#  "origin": "210.173.1.204",
+#
+#  "url": "https://httpbin.org/get"
+#
+# }
