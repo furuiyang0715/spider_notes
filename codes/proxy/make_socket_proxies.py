3def2af64fca60dc37f45b791e8239ba6236f736
furuiyang <furuiyang0715@gmail.com>
2021-03-01 13:49:19 +0800

    使用 socket 代理服务器

--- codes/proxy/make_socket_proxies.py
+++ codes/proxy/make_socket_proxies.py
@@ -1,17 +1,32 @@
+# import requests
+#
+# proxy = '127.0.0.1:7891'
+#
+# proxies = {
+#     'http': 'socks5://'+proxy,
+#     'https': 'socks5://'+proxy
+# }
+#
+# try:
+#     response = requests.get('https://httpbin.org/get',
+#                             # proxies=proxies,
+#                             )
+#     print(response.text)
+# except requests.exceptions.ConnectionError as e:
+#     print('Error', e.args)
+
+
+# pip3 install "requests[socks]"
+
+
 import requests
-
-proxy = '127.0.0.1:7891'
-
-proxies = {
-    'http': 'socks5://'+proxy,
-    'https': 'socks5://'+proxy
-}
+import socks     # 无法安装
+import socket
+socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 7891)
+socket.socket = socks.socksocket
 
 try:
-    response = requests.get('https://httpbin.org/get',
-                            # proxies=proxies,
-                            )
+    response = requests.get('https://httpbin.org/get')
     print(response.text)
 except requests.exceptions.ConnectionError as e:
     print('Error', e.args)
-
