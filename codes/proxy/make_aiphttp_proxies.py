5b595100a00d20f864fee996fd4a68d915505f18
furuiyang <furuiyang0715@gmail.com>
2021-03-01 14:20:47 +0800

    为 aiohttp 设置代理

--- codes/proxy/make_aiphttp_proxies.py
+++ codes/proxy/make_aiphttp_proxies.py
@@ -12,3 +12,21 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
+
+
+# pip3 install aiohttp-socks
+import asyncio
+import aiohttp
+from aiohttp_socks import ProxyConnector
+
+connector = ProxyConnector.from_url('socks5://127.0.0.1:7891')
+
+
+async def main():
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get('https://httpbin.org/get') as response:
+            print(await response.text())
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
