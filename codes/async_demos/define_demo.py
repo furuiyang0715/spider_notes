8b06956e86d6b44003926e22436975a93b3bb224
furuiyang <furuiyang0715@gmail.com>
2021-03-01 15:02:21 +0800

    ensure future 创建

--- codes/async_demos/define_demo.py
+++ codes/async_demos/define_demo.py
@@ -20,13 +20,24 @@ async def execute(x):
 # print('After calling loop')
 
 
+# coroutine = execute(1)
+# print('Coroutine:', coroutine)
+# print('After calling execute')
+# loop = asyncio.get_event_loop()
+# # 显式创建一个 task 对象
+# task = loop.create_task(coroutine)
+# print('Task:', task)
+# loop.run_until_complete(task)
+# print('Task:', task)
+# print('After calling loop')
+
+
 coroutine = execute(1)
 print('Coroutine:', coroutine)
 print('After calling execute')
-loop = asyncio.get_event_loop()
-# 显式创建一个 task 对象
-task = loop.create_task(coroutine)
+task = asyncio.ensure_future(coroutine)
 print('Task:', task)
+loop = asyncio.get_event_loop()
 loop.run_until_complete(task)
 print('Task:', task)
 print('After calling loop')
